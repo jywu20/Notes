7f355779b647c7eba34d3fe1f8b3e8993a1ec068
wujinq <wujinyuan111@126.com>
2021-12-19 12:22:30 +0800

    update physics notes

--- Physics-new/quantum-optics-homework/4/simulation.jl
+++ Physics-new/quantum-optics-homework/4/simulation.jl
@@ -15,27 +15,27 @@ C = [0 1; 0 0]
 σᶻ = - C' * C + C * C'
 σ⁰ = C' * C + C * C'
 
-Pee = (σ⁰ + σᶻ) / 2
+Pee = (σ⁰ - σᶻ) / 2
 Pgg = σ⁰ - Pee
 
-Γ = 2 * π * 0.0
+Γ = 2π * 1.0
 H_damping = - im * Γ * C' * C / 2
 
 t_max = 1.0
 Δt = 0.001
 t = 0.0 : Δt : t_max 
 
-n_traj = 500
+n_traj = 1000
 n_t = length(t)
 n_x = zeros(n_t, n_traj)
 n_y = zeros(n_t, n_traj)
 n_z = zeros(n_t, n_traj)
 
 # Note that both of Ω and Δ can be time dependent
-Ω = ones(n_t) * 2π * 5.0
-Δ = ones(n_t) * 2π * 0.0
+Ω = ones(n_t) * 2π * 0.0
+Δ = ones(n_t) * 2π * 5.0
 
-ψ_init = [0.0, 1.0] # / sqrt(2)
+ψ_init = [1.0, 1.0] / sqrt(2)
 
 progress = Progress(n_traj)
 
@@ -71,13 +71,18 @@ n_x_mean = mean(n_x, dims = 2)[:, 1]
 n_y_mean = mean(n_y, dims = 2)[:, 1]
 n_z_mean = mean(n_z, dims = 2)[:, 1]
 
-p = plot3d(n_x_mean, n_y_mean, n_z_mean, 
-    xlims = (-1.0, 1.0), ylims = (-1.0, 1.0), zlims = (-1.0, 1.0), legend = false, 
-    xlabel = L"\sigma_x", ylabel = L"\sigma_y", zlabel = L"\sigma_z", dpi = 500)
-
-scatter3d!(p, [0.0], [0.0], [0.0], color = "red")
+n_x_single = n_x[:, 1]
+n_y_single = n_y[:, 1]
+n_z_single = n_z[:, 1]
 
 ##
+# Bloch sphere 
+
+p = plot3d(n_x_mean, n_y_mean, n_z_mean, 
+    xlims = (-1.0, 1.0), ylims = (-1.0, 1.0), zlims = (-1.0, 1.0), label = "averaged", 
+    xlabel = L"\sigma_x", ylabel = L"\sigma_y", zlabel = L"\sigma_z", dpi = 500)
+plot3d!(p, n_x_single, n_y_single, n_z_single, label = "single")
+scatter3d!(p, [0.0], [0.0], [0.0], color = "red", label = false)
 
 # Be cautious, do not overwrite previous figures
 output_fig = false
@@ -88,3 +93,28 @@ fig_name = "start-z.png"
 if output_fig
     savefig(p, working_path * fig_name)    
 end
+
+p
+
+##
+# Occupation of the excited state
+
+pee_single = 1 / 2 .- n_z_single / 2
+pee_mean = 1 / 2 .- n_z_mean / 2
+
+p = plot(t, pee_single, label = "single", dpi = 500)
+plot!(p, t, pee_mean, label = "mean")
+
+title!(p, L"\Omega = 0, \ \ \Gamma = 2 \pi, \ \ \Delta = 10 \pi , \ \  | \psi(t=0) \rangle = (| \mathrm{g} \rangle + | \mathrm{e} \rangle) / \sqrt{2}")
+
+# Be cautious, do not overwrite previous figures
+output_fig = true
+
+working_path = "D:/Notes/Physics-new/quantum-optics-homework/4/"
+fig_name = "rwf-5.png"
+
+if output_fig
+    savefig(p, working_path * fig_name)    
+end
+
+p
