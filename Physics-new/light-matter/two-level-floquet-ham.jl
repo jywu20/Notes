4561db35f9ba87e37c8fcb07cb3e609e7dc4949f
wujinq <wujinyuan111@126.com>
2023-12-04 23:57:50 -0500

    update notes

--- Physics-new/light-matter/two-level-floquet-ham.jl
+++ Physics-new/light-matter/two-level-floquet-ham.jl
@@ -75,11 +75,46 @@ function floquet_subspace_convergence_hams(atom::TwoLevelFloquetAtom, N_max::Int
     end
 end
 
-# The Floquet spectrum v.s. the RWA spectrum
+######################################################
+#region Display eigensystem of Floquet and RWA Hamiltonians
+
 let E = 1.0,
-    Ω = 0.4, 
-    ω = 0.7, 
+    # The configuration below gives us a small gap 
+    #Ω = 0.2, 
+    #ω = 0.58, 
+    Ω = 0.2, 
+    ω = 0.3,
     N = 80
+    
+    Δ = E - ω 
+    atom = TwoLevelFloquetAtom(Δ, Ω, ω)
+
+    H_floquet = ham_floquet(atom, -N:N - 1)
+    H_RWA     = ham_RWA(atom)
+    
+    println("Floquet:")
+    display(H_floquet)
+    println("RWA:")
+    display(H_RWA)
+    println()
+    
+    E_floquet = sort(back_to_1BZ.(eigen(H_floquet).values, ω))
+    E_RWA     = sort(back_to_1BZ.(eigen(H_RWA).values    , ω))
+    display(E_floquet)
+    display(E_RWA)
+end
+
+#endregion
+######################################################
+
+######################################################
+#region The Floquet spectrum v.s. the RWA spectrum, quantitatively:
+# plots of eigensystem
+
+let E = 1.0,
+    Ω = 0.2, 
+    ω = 0.3, 
+    N = 200
     
     Δ = E - ω 
     atom = TwoLevelFloquetAtom(Δ, Ω, ω)
@@ -90,31 +125,49 @@ let E = 1.0,
     E_floquet = sort(back_to_1BZ.(eigen(H_floquet).values, ω))
     E_RWA     = sort(back_to_1BZ.(eigen(H_RWA).values    , ω))
     
-    display(H_floquet)
-
     p = scatter(E_floquet)
     hline!(p, E_RWA)
+    ylims!(p, (0, 1))
     p
 end
 
-# The Floquet spectrum v.s. the RWA spectrum, quantitatively:
-function floquet_rwa_diff(E, Ω, ω, N) 
+#endregion
+######################################################
+
+######################################################
+#region  The Floquet spectrum v.s. the RWA spectrum, quantitatively: heatmap
+
+function floquet_energy_level_align(ω, E1, E2; N=5)
+    displacements = collect(- N : N - 1) * ω
+    E1_displaced = map(Iterators.product(map(E -> E .+ displacements, E1)...)) do E1′
+        sort([E1′...])
+    end
+
+    energy_differences = (x -> norm(x)).(E1_displaced .- [E2]) 
+    E1_displaced[argmin(energy_differences)]
+end
+
+function floquet_rwa_diff(E, Ω, ω, N; ϵ = 0.01) 
     Δ = E - ω 
     atom = TwoLevelFloquetAtom(Δ, Ω, ω)
 
     H_floquet = ham_floquet(atom, -N : N - 1)
     H_RWA     = ham_RWA(atom)
     
-    E_floquet = sort(back_to_1BZ.(eigen(H_floquet).values, ω))
-    E_RWA     = sort(back_to_1BZ.(eigen(H_RWA).values    , ω))
+    E_floquet = sort(eigen(H_floquet).values)
+    E_RWA     = sort(eigen(H_RWA).values)  
     
-    E_RWA = vcat(ones(2N) * E_RWA[1], ones(2N) * E_RWA[2]) 
+    ΔE² = map(E_RWA) do E
+        minimum((E_floquet .- E).^2)
+    end
     
-    sqrt(mean((E_floquet - E_RWA).^2) / E) 
+    #E_average = map(x -> max(x, ϵ), E_average)
+    #mean(sqrt.(ΔE.^2) ./ E_average) 
+    sqrt(mean(ΔE² ./ E_RWA.^2)) 
 end
 
-@profview let Ω_list = LinRange(0, 2, 50), 
-    ω_list = LinRange(0, 2, 50)
+let Ω_list = LinRange(0, 2, 200), 
+    ω_list = LinRange(0, 2, 200)
     
     sqrt_mse_list = zeros(length(Ω_list), length(ω_list))
     progress = Progress(length(sqrt_mse_list))
@@ -133,11 +186,176 @@ end
     Δω    = step(ω_list)
     p = heatmap(Ω_list, ω_list, sqrt_mse_list', 
         aspect_ratio = :equal,
-        xlabel = L"\Omega",
-        ylabel = L"\omega",
+        xlabel = L"\Omega / \omega_{\mathrm{eg}}",
+        ylabel = L"\omega / \omega_{\mathrm{eg}}",
         xlims = (Ω_min - ΔΩ / 2, Ω_max + ΔΩ / 2),
         ylims = (ω_min - Δω / 2, ω_max + Δω / 2), 
-        clims = (0, 1))
+    )
     display(p)
     p
 end
+
+#endregion
+######################################################
+
+######################################################
+#region Fixed Ω
+
+let Ω_list = [0.2], 
+    ω_list = LinRange(0, 2, 5000)
+    
+    sqrt_mse_list = zeros(length(Ω_list), length(ω_list))
+    progress = Progress(length(sqrt_mse_list))
+    for (Ω_idx, Ω) in enumerate(Ω_list)
+        for (ω_idx, ω) in enumerate(ω_list)
+            sqrt_mse_list[Ω_idx, ω_idx] = floquet_rwa_diff(1.0, Ω, ω, 40)
+            next!(progress)
+        end
+    end
+    
+    p = plot(ω_list, sqrt_mse_list[1, :], 
+        xlabel = L"\omega / \omega_{\mathrm{eg}}")
+    display(p)
+    p
+end
+
+#endregion
+######################################################
+
+######################################################
+#region Fixed ω
+
+let Ω_list = LinRange(0, 2, 10000),
+    ω_list = [0.5]
+    
+    sqrt_mse_list = zeros(length(Ω_list), length(ω_list))
+    progress = Progress(length(sqrt_mse_list))
+    for (Ω_idx, Ω) in enumerate(Ω_list)
+        for (ω_idx, ω) in enumerate(ω_list)
+            sqrt_mse_list[Ω_idx, ω_idx] = floquet_rwa_diff(1.0, Ω, ω, 40)
+            next!(progress)
+        end
+    end
+    
+    p = plot(Ω_list, sqrt_mse_list[:, 1], 
+        xlabel = L"\Omega / \omega_{\mathrm{eg}}")
+    p
+end
+
+#endregion
+######################################################
+
+######################################################
+#region Fixed ω, plot band gap 
+
+let Ω_list = LinRange(0, 2, 100),
+    ω_list = LinRange(0, 2, 200),    
+    E = 1.0, 
+    N = 10
+    
+    gap_list = zeros(length(Ω_list), length(ω_list))
+    progress = Progress(length(gap_list))
+    for (Ω_idx, Ω) in enumerate(Ω_list)
+        for (ω_idx, ω) in enumerate(ω_list)
+            Δ = E - ω
+            sys = TwoLevelFloquetAtom(Δ, Ω, ω)
+            H_RWA = ham_RWA(sys)
+            E_RWA = back_to_1BZ.(eigen(H_RWA).values, ω)
+            gap_list[Ω_idx, ω_idx] = abs(E_RWA[1] - E_RWA[2]) 
+            next!(progress)
+        end
+    end
+    
+    Ω_min = minimum(Ω_list)
+    Ω_max = maximum(Ω_list)
+    ΔΩ    = step(Ω_list)
+    ω_min = minimum(ω_list)
+    ω_max = maximum(ω_list)
+    Δω    = step(ω_list)
+    p = heatmap(Ω_list, ω_list, gap_list', 
+        aspect_ratio = :equal,
+        xlabel = L"\Omega / \omega_{\mathrm{eg}}",
+        ylabel = L"\omega / \omega_{\mathrm{eg}}",
+        xlims = (Ω_min - ΔΩ / 2, Ω_max + ΔΩ / 2),
+        ylims = (ω_min - Δω / 2, ω_max + Δω / 2), 
+    )
+    display(p)
+    p
+end
+
+let Ω_list = LinRange(0, 2, 100),
+    ω_list = LinRange(0, 2, 200),    
+    E = 1.0, 
+    N = 10
+    
+    gap_list = zeros(length(Ω_list), length(ω_list))
+    progress = Progress(length(gap_list))
+    for (Ω_idx, Ω) in enumerate(Ω_list)
+        for (ω_idx, ω) in enumerate(ω_list)
+            Δ = E - ω
+            sys = TwoLevelFloquetAtom(Δ, Ω, ω)
+            H_RWA = ham_RWA(sys)
+            #E_RWA = back_to_1BZ.(eigen(H_RWA).values, ω)
+            # It seems the sole reason there are discontinuity in the heatmap 
+            # is because back_to_1BZ itself introduces discontinuity.
+            E_RWA = eigen(H_RWA).values
+            gap_list[Ω_idx, ω_idx] = abs(E_RWA[1] - E_RWA[2]) 
+            next!(progress)
+        end
+    end
+    
+    Ω_min = minimum(Ω_list)
+    Ω_max = maximum(Ω_list)
+    ΔΩ    = step(Ω_list)
+    ω_min = minimum(ω_list)
+    ω_max = maximum(ω_list)
+    Δω    = step(ω_list)
+    p = heatmap(Ω_list, ω_list, gap_list', 
+        aspect_ratio = :equal,
+        xlabel = L"\Omega / \omega_{\mathrm{eg}}",
+        ylabel = L"\omega / \omega_{\mathrm{eg}}",
+        xlims = (Ω_min - ΔΩ / 2, Ω_max + ΔΩ / 2),
+        ylims = (ω_min - Δω / 2, ω_max + Δω / 2), 
+    )
+    display(p)
+    p
+end
+
+let Ω_list = LinRange(0, 2, 100),
+    ω_list = LinRange(0, 2, 200),    
+    E = 1.0, 
+    N = 10
+    
+    gap_list = zeros(length(Ω_list), length(ω_list))
+    progress = Progress(length(gap_list))
+    for (Ω_idx, Ω) in enumerate(Ω_list)
+        for (ω_idx, ω) in enumerate(ω_list)
+            Δ = E - ω
+            sys = TwoLevelFloquetAtom(Δ, Ω, ω)
+            H_RWA = ham_RWA(sys)
+            #E_RWA = back_to_1BZ.(eigen(H_RWA).values, ω)
+            E_RWA = eigen(H_RWA).values
+            gap_list[Ω_idx, ω_idx] = minimum(E_RWA) 
+            next!(progress)
+        end
+    end
+    
+    Ω_min = minimum(Ω_list)
+    Ω_max = maximum(Ω_list)
+    ΔΩ    = step(Ω_list)
+    ω_min = minimum(ω_list)
+    ω_max = maximum(ω_list)
+    Δω    = step(ω_list)
+    p = heatmap(Ω_list, ω_list, gap_list', 
+        aspect_ratio = :equal,
+        xlabel = L"\Omega / \omega_{\mathrm{eg}}",
+        ylabel = L"\omega / \omega_{\mathrm{eg}}",
+        xlims = (Ω_min - ΔΩ / 2, Ω_max + ΔΩ / 2),
+        ylims = (ω_min - Δω / 2, ω_max + Δω / 2), 
+    )
+    display(p)
+    p
+end
+
+#endregion
+######################################################
